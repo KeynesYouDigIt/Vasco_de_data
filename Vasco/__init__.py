293eef52177d5eeb3ff3144a158a514f82006be8
Vincent Buscarello <vince.buscarello@gmail.com>
2016-07-01 23:59:29 -0600

    adding more heroku stuff

--- Vasco/__init__.py
+++ Vasco/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['DEBUG'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:52186vato@localhost:5432/Ocean'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ybanibhzpaamxs:_x5_Ly1sCCwGsWKIw_Kbvr4Cqi@localhost:5432/Ocean'
 
 #blueprints for moduarization of the app and its functions coming soon!
 
