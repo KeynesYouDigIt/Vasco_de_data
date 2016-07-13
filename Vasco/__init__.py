f3eac662618fbdf7a4f881f9cb998b2b3efa4be1
Vincent Buscarello <vince.buscarello@gmail.com>
2016-07-13 08:41:56 -0600

    debug mode off

--- Vasco/__init__.py
+++ Vasco/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ['DATABASE_URL']
 
