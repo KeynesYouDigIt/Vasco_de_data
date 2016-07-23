4dffd5cc7c3dd1de05016f8f06cace96984d257d
Vincent Buscarello <vince.buscarello@gmail.com>
2016-07-23 10:47:51 -0600

    more git ignore testing

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 import os
 
 from Vasco import *
@@ -8,31 +6,32 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
 #on switch
+app = create_app(os.getenv('SET_ENV') or 'prod')
 manager = Manager(app)
 
 #migrations
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
-# @manager.command
-# def list_routes():
-#     '''Taken from http://flask.pocoo.org/snippets/117/'''
-#     import urllib
-#     from flask import render_template, url_for
-#     output = []
-#     for rule in app.url_map.iter_rules():
+@manager.command
+def list_routes():
+    '''Taken from http://flask.pocoo.org/snippets/117/'''
+    import urllib
+    from flask import render_template, url_for
+    output = []
+    for rule in app.url_map.iter_rules():
 
-#         options = {}
-#         for arg in rule.arguments:
-#             options[arg] = "[{0}]".format(arg)
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
 
-#         methods = ','.join(rule.methods)
-#         url = url_for(rule.endpoint, **options)
-#         line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
-#         output.append(line)
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
     
-#     for line in sorted(output):
-#         print line
+    for line in sorted(output):
+        print line
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    manager.run(host='0.0.0.0', port=5000)
